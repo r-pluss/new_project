3e6959425c9aef24d2c409de92a094ef8389ee1c
r-pluss <r.pluss52@gmail.com>
2016-09-13 12:53:44 -0400

    Update corpus.py

--- python/corpus.py
+++ python/corpus.py
@@ -10,12 +10,16 @@ url = "https://www.wikidata.org/w/index.php?title=Special:WhatLinksHere/Property
 wiki_page = requests.get(url)
 if not wiki_page.ok:
     raise ValueError('Unable to get initial data from wikidata.org - {0} : {1}'.format(wiki_page.status_code, wiki_page.reason))
-html = bs4.Beautiful_Soup(wiki_page.content, 'html.parser')
+html = bs4.BeautifulSoup(wiki_page.content, 'html.parser')
 for item in hmtl.find(id = 'mw-whatlinkshere-list').find_all('li'):
     link = item.find('a').get('href')
     process(link)
     
     
 def process(url):
-    pass
-    
+    wiki_page = requests.get("https://www.wikidata.org{0}".format(url))
+    if not wiki_page.ok:
+        raise ValueError('Unable to get initial data from wikidata.org - {0} : {1}'.format(wiki_page.status_code, wiki_page.reason))
+    html = bs4.BeautifulSoup(wiki_page.content, 'html.parser')
+    #get the date of death
+    death_date = html.find(id = 'P570').find(class_ = 'wikibase-snakview-value').contents[0]
