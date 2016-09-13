6f14ba46c27edd7093b6965563eddae5a2305a62
r-pluss <r.pluss52@gmail.com>
2016-09-13 14:10:12 -0400

    Update corpus.py

--- python/corpus.py
+++ python/corpus.py
@@ -10,7 +10,7 @@ url = "https://www.wikidata.org/w/index.php?title=Special:WhatLinksHere/Property
 wiki_page = requests.get(url)
 if not wiki_page.ok:
     raise ValueError('Unable to get initial data from wikidata.org - {0} : {1}'.format(wiki_page.status_code, wiki_page.reason))
-html = bs4.BeautifulSoup(wiki_page.content, 'html.parser')
+html = bs4.BeautifulSoup(wiki_page.content, 'lxml')
 for item in hmtl.find(id = 'mw-whatlinkshere-list').find_all('li'):
     link = item.find('a').get('href')
     process(link)
@@ -20,6 +20,31 @@ def process(url):
     wiki_page = requests.get("https://www.wikidata.org{0}".format(url))
     if not wiki_page.ok:
         raise ValueError('Unable to get initial data from wikidata.org - {0} : {1}'.format(wiki_page.status_code, wiki_page.reason))
-    html = bs4.BeautifulSoup(wiki_page.content, 'html.parser')
+    html = bs4.BeautifulSoup(wiki_page.content, 'lxml')
     #get the date of death
     death_date = html.find(id = 'P570').find(class_ = 'wikibase-snakview-value').contents[0]
+    if death_date is None:
+        return
+    else:
+        #get the common name for the person
+        who_is = html.find(class_ = 'wikibase-title-label').text.strip()
+        #get the general description
+        descript = html.find(class_ = 'wikibase-entitytermsview-heading-description').text.strip()
+        #check for occupation properties to include as metadata
+        jobs = [item.text.strip() for item in
+            html.find(id = 'P106').find_all(class_ = 'wikibase-statementview-mainsnak')
+        ]
+        #see if there's an image to use
+        img_url = html.find(id = 'P18').find('a', class_ = 'extiw')['href']
+        if img_url[0:2] == '//':
+            img_url = 'http:' += img_url
+        #get the link for the person's wikipedia entry
+        wiki_link = html.find(class_ = 'wikibase-entityview-side').find('a', hreflang = 'en')['href']
+        #try to generate a ranking index value
+        #number of inter-wiki links will be used as a proxy for notoriety
+        _wiki_page = requests.get(wiki_link)
+        if not _wiki_page.ok:
+            return
+        
+        
+        
